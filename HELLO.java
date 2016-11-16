47af440d5d44e49b4b0ed85842ddd14b55ed0249
jmuletr <jmuletr@gmail.com>
2016-11-16 21:35:03 +0100

    canvi linea usuari 1

--- HELLO.java
+++ HELLO.java
@@ -1,6 +1,7 @@
 public class HELLO {
     public static void main(String[] args) {
         // La seguent linia serveix per imprimir la frase "Hola, Liceu"
+        //canvi a una linea
         System.out.println("Hola, Liceu");
         }
 }
