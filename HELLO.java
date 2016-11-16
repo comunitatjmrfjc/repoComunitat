6a5b6f1ae8364359a5d86b097ec93220d6d4224e
javi comas <franciscojaviercomas@gmail.com>
2016-11-16 21:33:04 +0100

    segon commit

---
+++ HELLO.java
@@ -0,0 +1,6 @@
+public class HELLO {
+    public static void main(String[] args) {
+        // La seguent linia serveix per imprimir la frase "Hola, Liceu"
+        System.out.println("Hola, Liceu");
+        }
+}
